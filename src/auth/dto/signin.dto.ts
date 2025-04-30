88aa509b0976939742b19a56ca3307b16aa82883
Luc4sL4idens <lucaslaidens20@gmail.com>
2025-04-29 22:02:51 -0300

    :sparkles: insert validation

--- src/auth/dto/signin.dto.ts
+++ src/auth/dto/signin.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty } from "class-validator";
+
 export class SigninDto {
+    @IsNotEmpty()
     cpf: string;
+
+    @IsNotEmpty()
     password: string;
   }
--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,6 +1,16 @@
+import { IsNotEmpty } from "class-validator";
+
+
 export class SignupDto {
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
   cpf: string;
+
+  @IsNotEmpty()
   password: string;
+
+  @IsNotEmpty()
   type: number;
 }
